958dc9bdef8412fec5946c201a6db3f8e213d8fe
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-03-17 12:52:41 -0300

    Ecercícios para aprender os operadores

---
+++ Sintaxe com Python/operadores_exercicios.py
@@ -0,0 +1,21 @@
+# Exercícios para aprender tipos de Operadores.
+
+# Exercício_1:
+
+# Exercício_2:
+
+# Exercício_3:
+
+# Exercício_4:
+
+# Exercício_5:
+
+# Exercício_6:
+
+# Exercício_7:
+
+# Exercício_8:
+
+# Exercício_9:
+
+# Exercício_10:
