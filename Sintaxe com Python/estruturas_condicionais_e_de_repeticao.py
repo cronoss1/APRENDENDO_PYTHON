e6599a9e677e0ab6f5e536c7bd0d40debaf699b7
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-03-18 15:48:25 -0300

    laços de repetição

--- Sintaxe com Python/estruturas_condicionais_e_de_repeticao.py
+++ Sintaxe com Python/estruturas_condicionais_e_de_repeticao.py
@@ -20,7 +20,8 @@ sacar(500) # Fim do bloco do método
 # Estruturas Condicionais:
 # A estrutura condicional permite o desvio de fluxo de controle,
 # quando determinada expressões lógicas são atendidas.
-# Exemplo (if): função (se)
+
+# Exemplo (if): Função (if/se)
 saldo = 2000.0 # Variavel
 saque = float(input("Informe o valor de saque: "))
 
@@ -29,5 +30,48 @@ if saldo >= saque: # Se saldo for maior ou igual ao saque (Realiazndo saque!).
 if saldo < saque: # Se saldo for menor do que saque (Saldo Insuficiente).
     print("Saldo Insuficiente!")
 
+# Exemplo (if/else): Função (if/se e else/ se não)
+saldo = 2000.0 # Variavel
+saque = float(input("Informe o valor de saque: "))
+
+if saldo >= saque: # Se saldo for maior ou igual ao saque (Realiazndo saque!).
+    print("Realizando saque!")
+else: # Se não atender ao requisito do if executa o else (Saldo Insuficiente).
+    print("Saldo Insuficiente!")
+
+# Exemplo (if/elif/else): Função (if/se, elif/se não se, elif/se não)
+opcao = int(input("Informe uma opção: [1] Sacar \n[2] Extrato: "))
+
+if opcao == 1:
+    valor = float(input("Informe a quantia para o saque: "))
+elif opcao == 2:
+    print("Exibindo o extrato...")
+else:
+    print("Opção inválida")
 
 # Estruturas de Repetição
+# São estruturas utilizadas para repetir um trecho de código um determinado número de vezes.
+# Esse número pode ser conhecido previamente ou determinado através de uma expressão lógica.
+# Exemplo (for): E usado quando sabemos o número exato de vezes que nosso bloco de código
+# deve ser executado.
+texto = input("Informe um texto:") # Variavel
+VOGAIS = "AEIOU" # Laço de repetição
+
+for letra in texto:
+    # Transformando letra para maiusculo.
+    if letra.upper() in VOGAIS:
+        print(letra, end="") # adiciona uma quebra de linha
+print()
+
+# Exemplo (for/else): else executa depois do laço de repetição
+texto = input("Informe um texto:") # Variavel
+VOGAIS = "AEIOU" # Laço de repetição
+
+for letra in texto:
+    # Transformando letra para maiusculo.
+    if letra.upper() in VOGAIS:
+        print(letra, end="") # adiciona uma quebra de linha
+else:
+    print("Executado depois do laço")
+
+# Exemplo (range): Usado para produzir uma sequência de números.
