fde3022c70a76500b98ea1f655f9c3a6f33825ba
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-04-03 11:08:30 -0300

    upadate lista

--- 2_Estruturas de Dados com Python/1_lista_em_python.py
+++ 2_Estruturas de Dados com Python/1_lista_em_python.py
@@ -101,26 +101,98 @@ numeros_2 = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros_2 if numero % 2 == 0]
 
 # Métodos da Classe list:
+
 # [].append - Adiciona um novo elemento na lista
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
 
 # [].clear - Utilizado quando eu quero limpar a minha lista
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
 
 # [].copy - Ele vai retornar uma copia da lista que não afeta a original
+lista = [1, "Python", [40, 30, 20]]
+
+lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
 
 # [].count - Usando para contar quantas vezes um objeto aparece na lista
+cores = ["vermelho", "azul", "verde", "azul"]
+
+cores.count("vermelho")  # 1
+cores.count("azul")  # 2
+cores.count("verde")  # 1
 
 # [].extend - Usado para adicionar mais de 1 elemento na lista 
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
 
 # [].index - Usado para saber o index da primeira vez que ele aparece na lista
+linguagens = ["python", "js", "c", "java", "csharp"]
 
-# [].pop - 
+linguagens.index("java")  # 3
+linguagens.index("python")  # 0
 
-# [].remove - 
+# [].pop - Usado para retirar o ultimo elemento da lista
+linguagens = ["python", "js", "c", "java", "csharp"]
 
-# [].reverse - 
+linguagens.pop()  # csharp
+linguagens.pop()  # java
+linguagens.pop()  # c
+linguagens.pop(0)  # python
 
-# [].sort - 
+# [].remove - Segunda forma de retirar um elemente da lista, passando o elemento
+linguagens = ["python", "js", "c", "java", "csharp"]
 
-# len - 
+linguagens.remove("c")
 
-# sorted - 
+print(linguagens)  # ["python", "js", "java", "csharp"]
+
+# [].reverse - Usado para fazer o espelhamento da lista
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.reverse()
+
+print(linguagens)  # ["csharp", "java", "c", "js", "python"]
+
+# [].sort - Usado para ordenar a lista, Ex: alfabeticamente, entrada e por tamanho crescente ou decrescente
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+
+# len - Usado para saber o tamanho da lista (quantos elementos ela tem)
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+len(linguagens)  # 5
+
+# sorted - Também usado para ordenar a lista
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+
