dd166facf48c89f7bba68c688a21e57cafee65f2
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-04-05 12:14:43 -0300

    update aulas de data e hora
    aulas de como manipular data e hora em python.

--- 2_Estruturas de Dados com Python/5_data_e_hora.py
+++ 2_Estruturas de Dados com Python/5_data_e_hora.py
@@ -1,1 +1,41 @@
 # MANIPULAÇÃO DE DATAS E HORAS
+
+"""
+O que é o módulo datetime?:
+O módulo 'datetime' em Python é usado para lidar com datas e horas. 
+Ele possui várias classes úteis como date, time e timedelta.
+"""
+# Exemplo:
+
+
+"""
+Introdução Manipulação de datas e horas:
+Podemos criar e manipular objetos date, time e datetime de várias maneiras. 
+Por exemplo, podemos adicionar e subtrair datas, verificar a diferença entre datas e muito mais.
+"""
+# Exemplo:
+
+
+"""
+Introdução Conversão e formatação de datas e horas:
+Python também permite converter e formatar datas e horas. 
+Para isso, usamos os métodos 'strftime' (string format time) e 'strptime' (string parse time).
+"""
+# Exemplo:
+
+
+"""
+Introdução Trabalhando com timezones:
+Quando trabalhamos com data e hora, lidar com fusos horários é uma necessidade comum. 
+Python facilita isso através do módulo 'pytz'.
+"""
+# Exemplo:
+
+
+"""
+Trabalhando com tz sem bibliotecas externas:
+O Python permite fazer isso com o módulo datetime padrão, embora seja um pouco mais 
+complexo do que usando bibliotecas como 'pytz'.
+"""
+# Exemplo:
+
