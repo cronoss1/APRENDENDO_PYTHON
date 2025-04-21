9236ec95477591992c8950d47847c9805e3872eb
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-04-21 09:53:09 -0400

    exercícios de operadores

---
+++ Exercicios/Sintaxe/01_Ex_operadores.py
@@ -0,0 +1,20 @@
+# Exercícios de nível 1:
+
+# EX_1:
+"""
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite mais um numero: "))
+soma = numero_1 + numero_2
+print(f"Resultado da soma: {soma}")
+"""
+# EX_2:
+"""
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite mais um numero: "))
+subtracao = numero_1 - numero_2
+if subtracao < 0:
+    print(f"O Resultado da subtração é positivo: {subtracao}")
+else:
+    print(f"O Resultado da subtração é negativo: {subtracao}")
+"""
+# EX_3:
