0a5ae1d2c9dd8d26ef77c5c08517c8737ddd91b6
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-04-22 16:02:04 -0300

    update exercicios

--- Exercicios/Sintaxe/Operadores/02_Ex_operadores.py
+++ Exercicios/Sintaxe/Operadores/02_Ex_operadores.py
@@ -1,8 +1,23 @@
 # Exercícios de nível 2:
 
 # EX_6:
-
+"""
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+if numero_1 > numero_2:
+    print(f"{numero_1} é maior que {numero_2}")
+elif numero_1 < numero_2:
+    print(f"{numero_2} é maior que {numero_1}")
+else:
+    print(f"{numero_1} é igual a {numero_2}")
+"""
 # EX_7:
+n_1 = 12
+n_2 = "12"
+if n_1 == n_2 and type(n_1) == type(n_2):
+    print(type(n_1) == type(n_2))
+else:
+    print("Diferentes")
 
 # EX_8:
 
