7a5b1a2a413b4ba580d8e2480ff4bd6c00415ea5
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-05-05 15:35:15 -0300

    exercicios nivel 3
    Exercícios nível 3 parcialmente resolvidos

--- Exercicios/Sintaxe/Condicionais/03_Ex_condicionais.py
+++ Exercicios/Sintaxe/Condicionais/03_Ex_condicionais.py
@@ -1,11 +1,46 @@
 # Exercícios de nível 3:
 
 # EX_11:
+"""
+nota = float(input("Digite a nota do aluno: "))
 
+if nota >= 5:
+    if nota >= 9:
+        print("Aluno tirou nota A")
+    elif nota >= 7:
+        print("Aluno tirou nota B")
+    elif nota >= 5:
+        print("Aluno tirou nota C")
+else:
+    print("Aluno reprovado")
+"""
 # EX_12:
+"""
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura: "))
+imc = peso / (altura ** 2)
 
+if imc >= 18.5:
+    if imc >= 30 and imc <= 34.9:
+        print(f"Você tem um imc de {imc:.2f}, obesidade tipo 1 ")
+    elif imc >= 25 and imc <= 29.9:
+        print(f"Você tem um imc de {imc:.2f}, sobrepeso")
+    elif imc >= 18.5 and imc <= 24.9:
+        print(f"Você tem um imc de {imc:2f}, peso normal")
+else:
+    print(f"Você tem um imc de {imc:.2f}, abaixo do peso")
+"""
 # EX_13:
+"""
+dia = int(input("Digite o dia da semana: "))
 
+if dia > 1 and dia < 7:
+    print("Dia util")
+elif dia == 1 or dia == 7:
+    print("Final de semana")
+else:
+    print("Dia invalido")
+""" 
 # EX_14:
 
 # EX_15:
