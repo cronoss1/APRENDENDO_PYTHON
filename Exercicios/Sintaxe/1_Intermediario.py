69b8b768200234d4f4cdc6f55cb5c4610de877db
cronosss <63974052+cronoss1@users.noreply.github.com>
2025-05-28 13:39:28 -0300

    Exercios de nível intermediario

---
+++ Exercicios/Sintaxe/1_Intermediario.py
@@ -0,0 +1,19 @@
+# Ex_1: Par ou ímpar com Validação
+
+# Ex_2: Calculadora com Operações Múltiplas
+
+# Ex_3: Maior e Menor em Três
+
+# Ex_4:Conversor de Temperatura
+
+# Ex_5: Número Primo (até 100)
+
+# Ex_6: Tabuada com Escolha
+
+# Ex_7: Contagem Regressiva com (while)
+
+# Ex_8: Conversor de Horas para Minutos e Segundos
+
+# Ex_9: Simulador de Caixa Eletrônica (troco)
+
+# Ex_10: Validador de CPF Simples
