5b2c6d5b892b8cb9c7a737ee6008aee4054fc4cb
cronoss1 <kauanlopes282@gmail.com>
2025-10-25 14:33:57 -0400

    Solução do Ex_1.py

--- Exercicios/Reais_01/Ex_1.py
+++ Exercicios/Reais_01/Ex_1.py
@@ -10,7 +10,18 @@ def validar_email(email):
     Retorna True se válido, False caso contrário
     """
     # Sua solução aqui
-    
+    if "@" not in email: # Verifica se tem arroba
+        return False
+    if " " in email: # Veirifica se tem espaços
+        return False
+    dominios_validos = [".com", ".br", ".org", ".com.br"] # Domínios válidos
+    if not any(dominio in email for dominio in dominios_validos): # Verifica domínio válido
+        # any() retorna True se algum dos domínios for encontrado
+        return False
+    if email.find("@") >= 3: # Verifica tamanho antes do '@'
+        return True
+    else: # Menos de 3 caracteres antes do '@'
+        return False
     pass
 
 # Teste suas soluções:
